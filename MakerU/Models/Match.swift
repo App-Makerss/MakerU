13602a4e157bfeb4673ccb4130712a32d8dd254d
Dibas <patia.mello@gmail.com>
2020-10-08 13:29:52 -0300

    Match

---
+++ MakerU/Models/Match.swift
@@ -0,0 +1,20 @@
+//
+//  Match.swift
+//  MakerU
+//
+//  Created by Patricia Amado Ferreira de Mello on 08/10/20.
+//
+
+import Foundation
+
+class Match {
+    var isMutual: Int64
+    var part1: String
+    var part2: String
+    
+    init(isMutual: Int64, part1: String, part2: String) {
+        self.isMutual = isMutual
+        self.part1 = part1
+        self.part2 = part2
+    }
+}
