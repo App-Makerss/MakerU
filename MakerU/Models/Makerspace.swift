c470cd0326e5470709b94da127d94ae756e2555f
BrunoCA-xD <bruno.car.ambrosio@gmail.com>
2020-10-08 15:21:53 -0300

    Done, only the model

---
+++ MakerU/Models/Makerspace.swift
@@ -0,0 +1,24 @@
+//
+//  Makerspace.swift
+//  MakerU
+//
+//  Created by Bruno Cardoso Ambrosio on 08/10/20.
+//
+
+import Foundation
+
+struct Makerspace: IdentifiableEntity, Codable {
+    var id: String?
+    
+    var name: String
+    var localization: Coordinate
+    var manager: String
+    var maxCapacity: Int
+    
+}
+
+
+struct Coordinate: Codable {
+    var latitude: Double
+    var longitude: Double
+}
