b2c3d10d5c811fa0dbdae059f4b111bebe2b29e3
BrunoCA-xD <bruno.car.ambrosio@gmail.com>
2020-10-13 11:17:47 -0300

    Added MatchDisplayConfigurationTableViewController and linked it to the matchViewController

--- MakerU/Controllers/MatchViewController.swift
+++ MakerU/Controllers/MatchViewController.swift
@@ -8,11 +8,27 @@
 import UIKit
 
 class MatchViewController: UIViewController {
+    
+    let configDisplayButton: UIButton = {
+        let btn = UIButton(type: .custom)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setTitle("Configurações de exibição", for: .normal)
+        btn.setTitleColor(.label, for: .normal)
+        btn.backgroundColor = .systemBlue
+        btn.addTarget(self, action: #selector(Self.configDisplayButtonTapped), for: .touchUpInside)
+        return btn
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.addSubview(configDisplayButton)
+        view.backgroundColor = .systemBackground
+        
+        configDisplayButton.heightAnchor.constraint(equalToConstant: 55).isActive = true
+        configDisplayButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        configDisplayButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
+        configDisplayButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -99).isActive = true
         
         setupNavigations()
     }
@@ -23,6 +39,11 @@ class MatchViewController: UIViewController {
         item.title = "Match"
         item.image = UIImage(systemName: "square.fill.on.square.fill")
         self.tabBarItem = item
+    }
+    
+    @objc func configDisplayButtonTapped() {
+        let displayConfigurationVC = MatchDisplayConfigurationTableViewController()
+        navigationController?.pushViewController(displayConfigurationVC, animated: true)
     }
     /*
     // MARK: - Navigation
