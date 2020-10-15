4e0dcbda6dbdec396aee6c6a41dff40a9ed21b8e
Victoria Faria <victoria.faria@ifood.com.br>
2020-10-14 23:46:44 -0300

    Create components

--- MakerU/Views/CollectionViewCells/MatchCardCollectionViewCell.swift
+++ MakerU/Views/CollectionViewCells/MatchCardCollectionViewCell.swift
@@ -8,5 +8,34 @@
 import UIKit
 
 class MatchCardCollectionViewCell: UICollectionViewCell {
-    
+
+    let headerHStack: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+
+        return view
+    }()
+
+    let cardTitle: UILabel = {
+        let title = UILabel()
+        title.font = UIFont.preferredFont(forTextStyle: .title1)
+        title.text = "Realidade aumentada para inclusão"
+        return title
+    }()
+
+    let cardSubtitle: UILabel = {
+        let subtitle = UILabel()
+        subtitle.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        subtitle.text = "Tecnologia"
+        return subtitle
+    }()
+
+    let shareButton: UIButton = {
+        let btn = UIButton(type: .custom)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
+        btn.setTitleColor(.label, for: .normal)
+        return btn
+    }()
+
 }
