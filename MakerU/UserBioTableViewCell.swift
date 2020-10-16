30fac4e3fffe2380ae8d421388c7968daa31d5cc
Dibas <patia.mello@gmail.com>
2020-10-15 22:04:19 -0300

    User Bio XIB

---
+++ MakerU/UserBioTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  UserBioTableViewCell.swift
+//  MakerU
+//
+//  Created by Patricia Amado Ferreira de Mello on 15/10/20.
+//
+
+import UIKit
+
+class UserBioTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
