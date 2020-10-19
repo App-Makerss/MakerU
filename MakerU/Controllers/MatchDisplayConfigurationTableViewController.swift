4a4e5341ce7df8e08487317976fd66ef475ed9aa
BrunoCA-xD <bruno.car.ambrosio@gmail.com>
2020-10-19 14:50:49 -0300

    Adding cells to tableView

--- MakerU/Controllers/MatchDisplayConfigurationTableViewController.swift
+++ MakerU/Controllers/MatchDisplayConfigurationTableViewController.swift
@@ -8,10 +8,51 @@
 import UIKit
 
 class MatchDisplayConfigurationTableViewController: UITableViewController {
+    
+    let configSegmentedControl: UISegmentedControl = {
+        let segmented = UISegmentedControl(items: ["Perfil", "Projeto"])
+
+        return segmented
+    }()
+    let rightBarButtonItem: UIBarButtonItem = {
+        let barButtonItem = UIBarButtonItem(title: "OK", style: .plain, target: self, action: nil)
+        barButtonItem.tintColor = UIColor.systemPurple
+        return barButtonItem
+    }()
+    
+    let leftBarButtonItem: UIBarButtonItem = {
+        let barButtonItem = UIBarButtonItem(title: "Cancelar", style: .plain, target: self, action: nil)
+        barButtonItem.tintColor = UIColor.systemPurple
+        return barButtonItem
+    }()
+    private func initFromNib(xibName: String, tableviewCell: UITableViewCell) {
+        if let nib = Bundle.main.loadNibNamed( xibName, owner: self, options: nil),
+           let nibView = nib.first as? UIView {
+            nibView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            tableviewCell.contentView.addSubview(nibView)
+            nibView.translatesAutoresizingMaskIntoConstraints = false
+            nibView.topAnchor.constraint(equalTo: tableviewCell.contentView.topAnchor).isActive = true
+            nibView.leadingAnchor.constraint(equalTo: tableviewCell.contentView.leadingAnchor).isActive = true
+            nibView.trailingAnchor.constraint(equalTo: tableviewCell.contentView.trailingAnchor).isActive = true
+            nibView.bottomAnchor.constraint(equalTo: tableviewCell.contentView.bottomAnchor).isActive = true
+        }
+    }
+    func setupNavigations() {
+        navigationItem.title = "Exibição"
+        
+        self.navigationItem.rightBarButtonItem = rightBarButtonItem
+        self.navigationItem.leftBarButtonItem = leftBarButtonItem
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.backgroundColor = .systemGray6
+        tableView.backgroundColor = .systemGray6
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupNavigations()
+        tableView.tableFooterView = UIView()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -20,70 +61,46 @@ class MatchDisplayConfigurationTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    override func tableView( _ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        let row = indexPath.row
+        
+        switch row {
+            case 0:
+                let cell = UITableViewCell()
+                cell.contentView.addSubview(configSegmentedControl)
+                return cell
+            case 1 :
+                let cell = UITableViewCell()
+                self.initFromNib(xibName: "UserProfileTagTableViewCell", tableviewCell: cell)
+                return cell
+                
+            case 2:
+                let cell = UITableViewCell()
+                self.initFromNib(xibName: "UserBioTableViewCell", tableviewCell: cell)
+                return cell
+                
+            case 3:
+                let cell = UITableViewCell()
+                self.initFromNib(xibName: "SkillsTableViewCell", tableviewCell: cell)
+                return cell
+            case 4:
+                let cell = UITableViewCell()
+                self.initFromNib(xibName: "ShowProfileTableViewCell", tableviewCell: cell)
+                return cell
+            default:
+                return UITableViewCell()
+        }
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
