bdf43657b5165e5ca49a54aac5c840ec533ada74
BrunoCA-xD <bruno.car.ambrosio@gmail.com>
2020-10-26 13:27:05 -0300

    re added UILabel+Extensions reference

--- MakerU/Extensions/UIFont+Extensions.swift
+++ MakerU/Extensions/UIFont+Extensions.swift
@@ -1,18 +1,36 @@
 //
-//  UIFont+Extensions.swift
+//  UILabel+Extensions.swift
 //  MakerU
 //
-//  Created by Bruno Cardoso Ambrosio on 16/10/20.
+//  Created by Bruno Cardoso Ambrosio on 24/10/20.
 //
 
+import Foundation
 import UIKit
 
-extension UIFont {
-    static func systemFont(style: UIFont.TextStyle, weight: UIFont.Weight? = nil) -> UIFont {
-
-        guard let weight = weight else {
-            return UIFont.preferredFont(forTextStyle: style)
-        }
-        return UIFont.systemFont(ofSize: UIFont.preferredFont(forTextStyle: style).pointSize, weight: weight)
+extension UILabel {
+    
+    var isTruncated: Bool {
+        guard let labelText = text, bounds.size.height > 0 else {return false}
+        let labelTextSize = (labelText as NSString).boundingRect(
+            with: CGSize(width: frame.size.width, height: .greatestFiniteMagnitude),
+            options: .usesLineFragmentOrigin,
+            attributes: [.font: font!],
+            context: nil).size
+        
+        return labelTextSize.height > bounds.size.height
     }
+    
+    func setDynamicType(font: UIFont, textStyle: UIFont.TextStyle? = nil){
+        self.adjustsFontForContentSizeCategory = true
+        var style: UIFont.TextStyle!
+        if textStyle == nil {
+            style = font.textStyle
+        }else {
+            style = textStyle
+        }
+        self.font = UIFontMetrics(forTextStyle: style).scaledFont(for: font,maximumPointSize: UIFont.maximumSize(for: style))
+    }
+    
 }
+
