8bda375545a5884e78ddb9649357aab7d1065770
Dibas <patia.mello@gmail.com>
2020-11-11 11:36:56 -0300

    Creating files

---
+++ MakerU/Controllers/LoginTitleViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginTitleViewController.swift
+//  MakerU
+//
+//  Created by Patricia Amado Ferreira de Mello on 11/11/20.
+//
+
+import Foundation
+import UIKit
+
+class LoginTitleViewController: UIViewController {
+    
+}
